bcc1d63a25459885eea33544657cd9819f2fb861
aditya777gh <adityagupta23555@gmail.com>
2026-08-19 12:02:12 +0530

    ok

---
+++ 1(Learn the basics)/1.2(Build-up Logical Thinking)/1.3(Patterns)/P1.java
@@ -0,0 +1,24 @@
+/*
+*****
+*****
+*****
+*****
+*****
+ */
+import java.util.*;
+class P1{
+    public static void main(String[] args){
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+        pattern1(n);
+    }
+
+    public static void pattern1(int n){
+        for(int i=0;i<n;i++){
+            for(int j=0;j<n;j++){
+                System.out.print("*");
+            }
+        System.out.println();
+        }
+    }
+}
